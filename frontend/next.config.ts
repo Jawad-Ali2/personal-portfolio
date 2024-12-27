1876b3cbf6df597cde151dbb5a51e6d567541456
Jawad Ali <107391644+Jawad-Ali2@users.noreply.github.com>
2024-12-28 01:26:09 +0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/personal-portfolio',
+  assetPrefix:"/personal-portfolio"
   /* config options here */
 };
 
