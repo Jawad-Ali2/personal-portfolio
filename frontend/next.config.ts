9ca4568b2e2edd7754a344a6dfc84e5919be916e
Jawad Ali <jawadali66667@gmail.com>
2024-12-28 01:42:58 +0500

    Update image path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/personal-portfolio',
-  assetPrefix:"/personal-portfolio"
   /* config options here */
 };
 
